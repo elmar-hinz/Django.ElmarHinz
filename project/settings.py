2c1f248f009b07312618f1a4da7ef2c0963c072b
Elmar Hinz <t3elmar@gmail.com>
2018-11-09 12:37:08 +0100

    Set DEBUG to False

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@i*5z+sj4dg@r7sh5_blsluoso259xx&wq(w4uo8vn0a%!4bx9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['elmarhinz.pythonanywhere.com']
 
